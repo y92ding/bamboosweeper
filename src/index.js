58053ebd933eb41dd1764d8d40d12b0fbe279fac
Yue Ding <yue@teis-MacBook-Pro.local>
2017-11-01 23:00:33 -0400

    add left click; recursively reveal broke

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css'
 
 class Square extends React.Component {
+  onLeftClick(e) {
+    e.preventDefault();
+    this.props.onLeftClick();
+  }
   render() {
     return (
-        <button className="square" onClick={this.props.onClick}>
+        <button className="square" onClick={this.props.onClick}
+            onContextMenu={(e)=>{this.onLeftClick(e)}}>
           {this.props.value}
         </button>
     );
@@ -19,7 +24,8 @@ class Grid extends React.Component {
       var row = [];
       for (let j = 0; j < this.props.size.width; j++) {
         row.push(<Square key={j} value={this.props.grid[i*10+j]}
-            onClick={() => this.props.onClickSquare(i*this.props.size.width+j)} />);
+            onClick={()=>this.props.onClickSquare(i*this.props.size.width+j)}
+            onLeftClick={()=>this.props.onLeftClickSquare(i*this.props.size.width+j)}/>);
       }
       grid.push(<div key={i} className="board-row">{row}</div>);
     }
@@ -47,20 +53,18 @@ class Game extends React.Component {
   }
 
   revealSquare(pos, playerGrid) {
-    console.log("revealing square "+pos.x+" "+pos.y);
     var y = pos.y;
     var x = pos.x;
     var width = this.state.size.width;
     var height = this.state.size.height;
     var arr = this.state.secretGrid;
-    if (arr[y*width+x] == 'x') {
+    if (arr[y*width+x] === 'x') {
       console.error("Calling revealSquare on a mine square...Abort!!!");
       return;
     }
 
     playerGrid[y*width+x] = arr[y*width+x];
-    console.log("revealed square "+x+" "+y+" is"+playerGrid[y*width+x]);
-    if (playerGrid[y*width+x] == '0') {
+    if (playerGrid[y*width+x] === '0') {
       if (y > 0 && x > 0 && playerGrid[(y-1)*width+x-1] == null) this.revealSquare({x: x-1, y: y-1}, playerGrid);
       if (y > 0 && playerGrid[(y-1)*width+x] == null) this.revealSquare({x: x, y: y-1}, playerGrid);
       if (y > 0 && x < width-1 && playerGrid[(y-1)*width+x+1] == null) this.revealSquare({x: x+1, y: y-1}, playerGrid);
@@ -75,7 +79,7 @@ class Game extends React.Component {
   onClickSquare(i) {
     var playerGrid = this.state.playerGrid.slice();
 
-    if (this.state.secretGrid[i] == 'x') {
+    if (this.state.secretGrid[i] === 'x') {
       alert("Clicked on mine!");
       return;
     }
@@ -88,10 +92,22 @@ class Game extends React.Component {
     });
   }
 
+  onLeftClickSquare(i) {
+    var playerGrid = this.state.playerGrid.slice();
+    playerGrid[i] = 'M';
+    this.setState({
+      playerGrid: playerGrid,
+    })
+  }
+
   render() {
     return (
+      <div>
       <Grid size={this.state.size} grid={this.state.playerGrid}
-          onClickSquare={(i) => this.onClickSquare(i)}/>
+          onClickSquare={(i) => this.onClickSquare(i)}
+          onLeftClickSquare={(i)=>this.onLeftClickSquare(i)}/>
+          <button onContextMenu={(e)=>{e.preventDefault()}} onMouseDown={(event)=>{console.log("you pressed button"+event.button)}}>Click</button>
+          </div>
     );
   }
 
@@ -107,19 +123,19 @@ function countSurroundingMines(pos, size, arr) {
   var x = pos.x;
   var width = size.width;
   var height = size.height;
-  if (arr[y*width+x] == 'x') {
+  if (arr[y*width+x] === 'x') {
     console.error("Counting surrounding mines for a mine square...Abort!!!");
     return -1;
   }
   var count = 0;
-  if (y > 0 && x > 0 && arr[(y-1)*width+x-1] == 'x') count++;
-  if (y > 0 && arr[(y-1)*width+x] == 'x') count++;
-  if (y > 0 && x < width-1 && arr[(y-1)*width+x+1] == 'x') count++;
-  if (x > 0 && arr[y*width+x-1] == 'x') count++;
-  if (x < width-1 && arr[y*width+x+1] == 'x') count++;
-  if (y < height-1 && x > 0 && arr[(y+1)*width+x-1] == 'x') count++;
-  if (y < height-1 && arr[(y+1)*width+x] == 'x') count++;
-  if (y < height-1 && x < width-1 && arr[(y+1)*width+x+1] == 'x') count++;
+  if (y > 0 && x > 0 && arr[(y-1)*width+x-1] === 'x') count++;
+  if (y > 0 && arr[(y-1)*width+x] === 'x') count++;
+  if (y > 0 && x < width-1 && arr[(y-1)*width+x+1] === 'x') count++;
+  if (x > 0 && arr[y*width+x-1] === 'x') count++;
+  if (x < width-1 && arr[y*width+x+1] === 'x') count++;
+  if (y < height-1 && x > 0 && arr[(y+1)*width+x-1] === 'x') count++;
+  if (y < height-1 && arr[(y+1)*width+x] === 'x') count++;
+  if (y < height-1 && x < width-1 && arr[(y+1)*width+x+1] === 'x') count++;
   return count;
 }
 
@@ -134,7 +150,7 @@ function makeSecretGrid(mines, size) {
   }
   for (let j = 0; j < size.height; j++) {
     for (let k = 0; k < size.width; k++) {
-      if (arr[j*size.width+k] != 'x') {
+      if (arr[j*size.width+k] !== 'x') {
         var surroundingMines = countSurroundingMines({x: k, y: j}, size, arr);
         arr[j*size.width+k] = surroundingMines;
       }
