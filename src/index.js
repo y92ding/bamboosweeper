c53e9b4f4bebaa413296698313c3dbaf0c7cc984
Yue <y92ding@edu.uwaterloo.ca>
2018-01-03 21:52:49 -0500

    update board according to the story

--- src/index.js
+++ src/index.js
@@ -19,20 +19,20 @@ class Square extends React.Component {
   }
 };
 
-class Grid extends React.Component {
+class Board extends React.Component {
   render() {
-    var grid = [];
+    var board = [];
     for (let i = 0; i < this.props.size.height; i++) {
       var row = [];
       for (let j = 0; j < this.props.size.width; j++) {
-        row.push(<Square key={j} value={this.props.grid[i*this.props.size.width+j]}
+        row.push(<Square key={j} value={this.props.board[i*this.props.size.width+j]}
             onClick={()=>this.props.onClickSquare(i*this.props.size.width+j)}
             onRightClick={()=>this.props.onRightClickSquare(i*this.props.size.width+j)}/>);
       }
-      grid.push(<div key={i} className="board-row">{row}</div>);
+      board.push(<div key={i} className="board-row">{row}</div>);
     }
 
-    return grid;
+    return board;
   }
 };
 
@@ -41,27 +41,25 @@ class SimpleGame extends React.Component {
     super(props);
     this.startTime;
     this.runTimer;
-    var secretGrid = this.makeSecretGrid(this.props.size);
+    var secretBoard = this.makeSecretBoard(this.props.size);
     var squareCount = this.props.size.width * this.props.size.height;
-    var caption = "Left click to reveal a grid, " +
-        "or right click to defuse a mine. Enjoy the game :)";
     this.state = {
-      playerGrid: Array(squareCount).fill(null),
-      secretGrid: secretGrid,
-      minesLeft: this.props.size.mines,
+      playerBoard: Array(squareCount).fill(null),
+      secretBoard: secretBoard,
+      hungerPoints: this.props.size.bamboos,
       status: "ready",
-      caption: caption,
+      caption: "Click 'Intro' above if you are not sure how to play. Enjoy the game :)",
       timer: 0.0,
     };
   }
 
-  countSurroundingMines(pos, size, arr) {
+  countSurroundingBamboos(pos, size, arr) {
     var y = pos.y;
     var x = pos.x;
     var width = size.width;
     var height = size.height;
     if (arr[y*width+x] === 'x') {
-      console.error("Counting surrounding mines for a mine square...Abort!!!");
+      console.error("Counting surrounding bamboos for a bamboo square...Abort!!!");
       return -1;
     }
     var count = 0;
@@ -76,10 +74,10 @@ class SimpleGame extends React.Component {
     return count;
   }
 
-  makeSecretGrid(size) {
+  makeSecretBoard(size) {
     var arr = Array(size.width*size.height).fill(null);
-    arr.fill('x', 0, size.mines);
-    for (let i = 0; i < size.mines; i++) {
+    arr.fill('x', 0, size.bamboos);
+    for (let i = 0; i < size.bamboos; i++) {
       let des = Math.floor(Math.random()*(size.width*size.height));
       console.log(des);
       let temp = arr[i];
@@ -89,8 +87,8 @@ class SimpleGame extends React.Component {
     for (let j = 0; j < size.height; j++) {
       for (let k = 0; k < size.width; k++) {
         if (arr[j*size.width+k] !== 'x') {
-          var surroundingMines = this.countSurroundingMines({x: k, y: j}, size, arr);
-          arr[j*size.width+k] = surroundingMines;
+          var surroundingBamboos = this.countSurroundingBamboos({x: k, y: j}, size, arr);
+          arr[j*size.width+k] = surroundingBamboos;
         }
       }
     }
@@ -105,7 +103,7 @@ class SimpleGame extends React.Component {
     })
   }
 
-  revealSquare(pos, playerGrid) {
+  revealSquare(pos, playerBoard) {
     if (this.state.status !== "playing" &&
         this.state.status !== "ready") {
       console.error("Calling revealSquare when status is not 'playing'...Abort!!!");
@@ -116,23 +114,23 @@ class SimpleGame extends React.Component {
     var x = pos.x;
     var width = this.props.size.width;
     var height = this.props.size.height;
-    var arr = this.state.secretGrid;
+    var arr = this.state.secretBoard;
     if (arr[y*width+x] === 'x') {
-      console.error("Calling revealSquare on a mine square...Abort!!!");
+      console.error("Calling revealSquare on a bamboo square...Abort!!!");
       return;
     }
 
-    playerGrid[y*width+x] = arr[y*width+x];
+    playerBoard[y*width+x] = arr[y*width+x];
 
-    if (playerGrid[y*width+x] === 0) {
-      if (y > 0 && x > 0 && playerGrid[(y-1)*width+x-1] == null) this.revealSquare({x: x-1, y: y-1}, playerGrid);
-      if (y > 0 && playerGrid[(y-1)*width+x] == null) this.revealSquare({x: x, y: y-1}, playerGrid);
-      if (y > 0 && x < width-1 && playerGrid[(y-1)*width+x+1] == null) this.revealSquare({x: x+1, y: y-1}, playerGrid);
-      if (x > 0 && playerGrid[y*width+x-1] == null) this.revealSquare({x: x-1, y: y}, playerGrid);
-      if (x < width-1 && playerGrid[y*width+x+1] == null) this.revealSquare({x: x+1, y: y}, playerGrid);
-      if (y < height-1 && x > 0 && playerGrid[(y+1)*width+x-1] == null) this.revealSquare({x: x-1, y: y+1}, playerGrid);
-      if (y < height-1 && playerGrid[(y+1)*width+x] == null) this.revealSquare({x: x, y: y+1}, playerGrid);
-      if (y < height-1 && x < width-1 && playerGrid[(y+1)*width+x+1] == null) this.revealSquare({x: x+1, y: y+1}, playerGrid);
+    if (playerBoard[y*width+x] === 0) {
+      if (y > 0 && x > 0 && playerBoard[(y-1)*width+x-1] == null) this.revealSquare({x: x-1, y: y-1}, playerBoard);
+      if (y > 0 && playerBoard[(y-1)*width+x] == null) this.revealSquare({x: x, y: y-1}, playerBoard);
+      if (y > 0 && x < width-1 && playerBoard[(y-1)*width+x+1] == null) this.revealSquare({x: x+1, y: y-1}, playerBoard);
+      if (x > 0 && playerBoard[y*width+x-1] == null) this.revealSquare({x: x-1, y: y}, playerBoard);
+      if (x < width-1 && playerBoard[y*width+x+1] == null) this.revealSquare({x: x+1, y: y}, playerBoard);
+      if (y < height-1 && x > 0 && playerBoard[(y+1)*width+x-1] == null) this.revealSquare({x: x-1, y: y+1}, playerBoard);
+      if (y < height-1 && playerBoard[(y+1)*width+x] == null) this.revealSquare({x: x, y: y+1}, playerBoard);
+      if (y < height-1 && x < width-1 && playerBoard[(y+1)*width+x+1] == null) this.revealSquare({x: x+1, y: y+1}, playerBoard);
     }
   }
 
@@ -150,29 +148,29 @@ class SimpleGame extends React.Component {
       return;
     }
 
-    if (this.state.playerGrid[i] !== null) {
+    if (this.state.playerBoard[i] !== null) {
       this.setState({
-        caption: "This grid is already revealed. Try another one.",
+        caption: "This square is already revealed. Try another one.",
       })
       return;
     }
 
-    if (this.state.secretGrid[i] === 'x') {
+    if (this.state.secretBoard[i] === 'x') {
       clearInterval(this.runTimer);
       this.setState({
         status: "lost",
-        caption: "You clicked on a mine! You lost. :(",
+        caption: "You let Brenda the panda step on a bamboo shoot! Brenda is hurt. :(",
       })
       return;
     }
 
-    var newPlayerGrid = this.state.playerGrid.slice();
+    var newplayerBoard = this.state.playerBoard.slice();
     var width = this.props.size.width;
-    this.revealSquare({x: i%width, y: Math.floor(i/width)}, newPlayerGrid);
+    this.revealSquare({x: i%width, y: Math.floor(i/width)}, newplayerBoard);
 
     this.setState({
-      playerGrid: newPlayerGrid,
-      caption: "There are " + this.state.minesLeft + " mines left.",
+      playerBoard: newplayerBoard,
+      caption: "Brenda the panda's hunger points: " + this.state.hungerPoints,
     });
   }
 
@@ -190,53 +188,53 @@ class SimpleGame extends React.Component {
       return;
     }
 
-    if (this.state.playerGrid[i] !== null) {
+    if (this.state.playerBoard[i] !== null) {
       this.setState({
-        caption: "This grid is already revealed. Try another one.",
+        caption: "This square is already revealed. Try another one.",
       })
       return;
     }
 
-    if (this.state.secretGrid[i] !== 'x') {
+    if (this.state.secretBoard[i] !== 'x') {
       clearInterval(this.runTimer);
       this.setState({
         status: "lost",
-        caption: "This is not a mine! You lost. :(",
+        caption: "This is not a bamboo shoot! Brenda is disappointed. :(",
       });
       return;
     }
 
-    var playerGrid = this.state.playerGrid.slice();
-    playerGrid[i] = 'M';
-    if (this.state.minesLeft <= 0) {
-      console.error("# of mines left already <= 0...Abort!!!");
+    var playerBoard = this.state.playerBoard.slice();
+    playerBoard[i] = '^';
+    if (this.state.hungerPoints <= 0) {
+      console.error("hunger points already <= 0...Abort!!!");
       return;
     }
-    var newMinesLeft = this.state.minesLeft - 1;
+    var newHungerPoints = this.state.hungerPoints - 1;
 
     this.setState({
-      playerGrid: playerGrid,
-      minesLeft: newMinesLeft,
+      playerBoard: playerBoard,
+      hungerPoints: newHungerPoints,
     })
 
-    if (newMinesLeft === 0) {
+    if (newHungerPoints === 0) {
       clearInterval(this.runTimer);
       this.setState({
         status: "won",
-        caption: "All mines are defused. You won!!! :D",
+        caption: "All bamboos are collected. You are Brenda's new friend now!!! :D",
       })
     } else {
       this.setState({
-        caption: "There are " + newMinesLeft + " mines left.",
+        caption: "Brenda the panda's hunger points: " + newHungerPoints,
       })
     }
   }
 
   render() {
     return (
-      <div className="grid">
+      <div className="board">
         <h3>{this.state.timer}</h3>
-        <Grid size={this.props.size} grid={this.state.playerGrid}
+        <Board size={this.props.size} board={this.state.playerBoard}
             onClickSquare={(i) => this.onClickSquare(i)}
             onRightClickSquare={(i)=>this.onRightClickSquare(i)}/>
         <h4>{this.state.caption}</h4>
@@ -251,10 +249,10 @@ class FancyGame extends React.Component {
     super(props);
     this.state = {
       view: 'intro',
-      gridSize: {
+      boardSize: {
         width: 15,
         height: 15,
-        mines: 30,
+        bamboos: 30,
       },
     };
     this.handleItemClick = this.handleItemClick.bind(this);
@@ -291,7 +289,7 @@ class FancyGame extends React.Component {
         );
         break;
       case 'game':
-        view = <SimpleGame className="grid" size={this.state.gridSize} />;
+        view = <SimpleGame className="board" size={this.state.boardSize} />;
         break;
       case 'rank':
         view = <p className="rank">rank</p>;
