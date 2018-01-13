ab4629c7f3a5c893ea8168325e61f9d6340c70d7
EC2 Default User <ec2-user@ip-172-31-30-30.us-west-2.compute.internal>
2018-01-13 19:24:03 +0000

    mongodb tryout

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
 
 app.listen('8080');
 
@@ -13,3 +14,11 @@ app.get('/testing', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'bamboosweeper-frontend/build/index.html'));
 });
+
+const url = "mongodb://yue:bamboosweeper@34.210.60.116:27017/bamboosweeper-db";
+MongoClient.connect(url, (err, db) => {
+  if (err) throw err;
+  console.log("Database connected!");
+  db.close();
+}); 
+
